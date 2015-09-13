0034e6dd6e4125be403af410c182066db9ffc04f
Kaustubh Sant <kaustubhsant10@gmail.com>
2015-09-13 18:34:47 -0400

    Adding digitalOcean script

---
+++ HW/HW1/digitalocean.js
@@ -0,0 +1,93 @@
+var needle = require("needle");
+var os   = require("os");
+var config = require('config');
+
+var token = config.get('token');
+
+var headers =
+{
+	'Content-Type':'application/json',
+	Authorization: 'Bearer ' + token
+};
+
+// Documentation for needle:
+// https://github.com/tomas/needle
+
+var client =
+{	
+
+	createDroplet: function ( onResponse)
+	{
+		var data = 
+		{
+			"name": "ksant-"+ os.hostname(),
+			"region": "nyc1",
+			"size":"512mb",
+			"image":"ubuntu-14-04-x32",
+			// Id to ssh_key already associated with account.			
+			"ssh_keys": config.get('ssh_keys'),
+			"backups":false,
+			"ipv6":false,
+			"user_data":null,
+			"private_networking":null
+		};
+
+		console.log("Attempting to create: "+ JSON.stringify(data) );
+
+		needle.post("https://api.digitalocean.com/v2/droplets", data, {headers:headers,json:true}, onResponse );
+	},
+
+	listDroplet: function( dropletId, onResponse )
+	{
+		needle.get("https://api.digitalocean.com/v2/droplets/"+dropletId, {headers:headers}, onResponse);
+	},
+
+	deletedroplet: function(dropletId, onResponse)
+	{
+		needle.delete("https://api.digitalocean.com/v2/droplets/"+dropletId,null, {headers:headers}, onResponse);	
+	}	
+};
+
+
+ client.createDroplet( function(err, resp, body)
+ {
+ 	var data = body;
+ 	// StatusCode 202 - Means server accepted request.
+ 	if(!err && resp.statusCode == 202)
+ 	{
+ 		console.log( JSON.stringify( body, null, 3 ) ); 		
+ 		setTimeout( function() {client.listDroplet(data.droplet.id, function(error,response) {
+			var data2 = response.body;
+	
+			if( data2.droplet )
+			{
+				console.log(data2.droplet);		
+				var ip_address = data2.droplet.networks.v4[0].ip_address;
+				console.log(ip_address);	
+			}
+		});},10000);
+ 		
+ 	}
+ });
+
+// #############################################
+// #6 Extend the client to DESTROY the specified droplet.
+// Comment out when done.
+// https://developers.digitalocean.com/#delete-a-droplet
+// HINT, use the DELETE verb.
+// HINT #2, needle.delete(url, data, options, callback), data needs passed as null.
+// No response body will be sent back, but the response code will indicate success.
+// Specifically, the response code will be a 204, which means that the action was successful with no returned body data.
+/*var dropletId = "7329980";
+client.deletedroplet(dropletId,function(err,resp){
+
+ 	if(!err && resp.statusCode == 204)
+ 	{
+			console.log("Deleted!");
+ 	}
+
+});*/
+// #############################################
+// #7 In the command line, ping your server, make sure it is dead!
+// ping xx.xx.xx.xx
+// It could be possible that digitalocean reallocated your IP address to another server, so don't fret it is still pinging.
