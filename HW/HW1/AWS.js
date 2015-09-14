661f92b19255059222826d01be0a9780a8f4f33f
Kaustubh Sant <kaustubhsant10@gmail.com>
2015-09-14 10:26:45 -0400

    Adding AWS provision code

---
+++ HW/HW1/AWS.js
@@ -0,0 +1,28 @@
+var os = require('os');
+var AWS = require('aws-sdk');
+
+AWS.config.loadFromPath('config/AWSconfig.json');
+
+var ec2 = new AWS.EC2();
+
+var params = {
+  ImageId: 'ami-1624987f', // Amazon Linux AMI x86_64 EBS
+  InstanceType: 't1.micro',
+  MinCount: 1, 
+  MaxCount: 1,
+  KeyName: "devops",
+  SecurityGroupIds: ["sg-c37b7aa4"],
+  SecurityGroups: ["devops"]
+};
+
+// Create the instance
+ec2.runInstances(params, function(err, data) {
+  if (err) { 
+  	console.log("Could not create instance", err); 
+  	return; 
+	}
+
+  var instanceId = data.Instances[0].InstanceId;
+  console.log("Created instance", instanceId);
+  
+});
